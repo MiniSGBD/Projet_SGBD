1049ddf05ce9cfeefea791738b14c2d12835b305
lilienne <yingle.xu@hotmail.fr>
2018-11-12 14:30:55 +0100

    TP2: Frame et BufferManager
    faire les méthodes de la classe Frame et BufferManager

--- Projet_Dai_Huang_Xu/Projet_Dai_Huang_Xu/Code/code/BufferManager.java
+++ Projet_Dai_Huang_Xu/Projet_Dai_Huang_Xu/Code/code/BufferManager.java
@@ -1,5 +1,9 @@
 package code;
 
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BufferManager {
 
 	private static final BufferManager INSTANCE = new BufferManager();
@@ -9,13 +13,63 @@ public class BufferManager {
 		return INSTANCE;
 	}
 
+	private Frame[] BufferPool = new Frame[Constantes.FRAMECOUNT];
+
 	/**
+	 * set instance
 	 * 
 	 * @param iNSTANCE
 	 */
 	public static void setINSTANCE(BufferManager iNSTANCE) {
 
 		iNSTANCE = INSTANCE;
+	}
+
+
+
+	/**
+	 * permet de retourne un des buffers qui stockent le contenu d¡¯une page dans
+	 * une des cases
+	 * 
+	 * @param iPageId???
+	 * @return un des buffers qui stockent le contenu d¡¯une page dans une des
+	 *         cases
+	 * return null: si aucune valeur correspond ¨¤ ce iPageId et il n'est pas possible de getPage
+	 */
+	public Byte[] getPage(PageId iPageId) {
+		for (int i = 0; i < Constantes.FRAMECOUNT; i++) {
+			if (BufferPool[i].getPageId() == iPageId){
+				BufferPool[i].increPinCount();
+				return BufferPool[i].getContenue();
+			}
+		}
+		for (int i = 0; i < Constantes.FRAMECOUNT; i++) {
+			if(BufferPool[i]==null) {
+				BufferPool[i]=new Frame(iPageId);
+				BufferPool[i].increPinCount();
+				return BufferPool[i].getContenue();
+			}
+		}
+		return null; // a changer
+
+	}
+
+	public void freePage(PageId iPageId, boolean iIsDirty) {
+		for (int i = 0; i < Constantes.FRAMECOUNT; i++) {
+			if (BufferPool[i].getPageId() == iPageId){
+				BufferPool[i].decrePinCount();
+				if (iIsDirty) BufferPool[i].setDirty(true);
+			}
+		}
+	}
+
+	public void flushAll() {
+		for (int i = 0; i < Constantes.FRAMECOUNT; i++) {
+			if (BufferPool[i].getDirty()==true){
+				BufferPool[i].setContenue(null);
+				BufferPool[i].setPinCount(0);
+			}
+		}
 	}
 
 }
--- Projet_Dai_Huang_Xu/Projet_Dai_Huang_Xu/Code/code/Frame.java
+++ Projet_Dai_Huang_Xu/Projet_Dai_Huang_Xu/Code/code/Frame.java
@@ -1,5 +1,56 @@
 package code;
 
+import java.nio.Buffer;
+
 public class Frame {
+
+	Byte [] contenue;
+	PageId pageId;
+	int pin_count;
+	boolean dirty;
+
+	public Frame(PageId pageId){
+		this.pageId=pageId;
+		pin_count=0;
+		dirty=false;
+	}
+
+	//les m¨¦thodes pour PinCount
+	public void increPinCount(){
+		pin_count++;
+	}
+	public void decrePinCount(){
+		pin_count--;
+	}
+	public void setPinCount(int valeur){
+		pin_count=valeur;
+	}
+	public int getPinCount(int valeur){
+		return(pin_count);
+	}
+	
+
+	//les m¨¦thodes pour Byte [] contenue
+	public void setContenue(Byte [] contenue){
+		this.contenue=contenue;
+	}
+	public Byte[] getContenue(){
+		return contenue;	
+	}
+	
+	//les m¨¦thodes pour Dirty
+	public void setDirty(Boolean iIsDirty){
+		 this.dirty=iIsDirty;
+	}
+	public boolean getDirty(){
+		return this.dirty;
+	}
+	
+	////les m¨¦thodes pour pageId
+	public PageId getPageId(){
+		return this.pageId;
+	}
+	
+
 
 }
